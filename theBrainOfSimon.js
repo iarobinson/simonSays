4cd156fb85711774e5a862b66a099db1cb1e4d9e
Ian <ian (at) ianrobinson.net>
2018-05-04 14:22:07 +0200

    Added fade effect to start + reset button so on hover the text fades

--- theBrainOfSimon.js
+++ theBrainOfSimon.js
@@ -9,12 +9,13 @@ function init() {
   addToGameSequence();
 }
 
+// What happens when a button is clicked
 function buttonClicked(color) {
-  unfade(color);
-  // Managing User Button Clicks Here
-  // Colors passed in as single letter strings related to color. Example Red = 'r'
-  console.log(color);
-  
+  if (listenToPlayer) {
+    console.log(typeof color, "<-typeof color", color, "<- color");
+    animate(color);
+    checkSequence();
+  }
 }
 
 function startOrStopGame() {
@@ -22,39 +23,67 @@ function startOrStopGame() {
 }
 
 function resetGame() {
-  // Reset Game
-  console.log('reset');
   gameSequence = [];
 }
 
 // This function plays the randomly selected sequence for user to watch
 function playSequence() {
-  console.log("playSequence");
-  for (var i = 0; i < gameSequence.length; i += 1) {
-    setTimeout(function () {
-      gameSequence[i];
-    }, 500);
+  var intervalId;
+  var runCount = 0;
+
+  function playSequence() {    
+    // Clear interval after gameSequence is played
+    if (runCount >= gameSequence.length - 1) {
+      clearInterval(intervalId);
+    }
+    
+    // The problem is here.
+    // I need to convert the string array element to a clickable element
+    var formatted = '<a onclick="buttonClicked(this);"><div id="' + gameSequence[runCount] + '" class="mainGameButton" style="display: block; opacity: 1.08347;"></a>';
+    
+    animate(formatted);
+    runCount += 1;
   }
+
+  var intervalId = setInterval(playSequence, 1000);
 }
 
 function addToGameSequence() {
   var avaliableColors = ['r', 'g', 'b', 'y'];
   var newColor = avaliableColors[Math.floor(Math.random() * avaliableColors.length)];
-  console.log(newColor, "<- newColor");
   gameSequence.push(newColor);
-  console.log(gameSequence, "<-gameSequence");
 }
 
-function unfade(element) {
-  console.log(element, "<-element");
-  var opacity = 0.1;  // initial opacity
-  element.style.display = 'block';
-  var timer = setInterval(function () {
-      if (opacity >= 1){
-          clearInterval(timer);
+// This will test playerSequence against gameSequence
+function checkSequence(playerSequence, gameSequence) {
+  if (playerSequence.length === 0) {
+    return true;
+  } else {
+    for (var i = 0; i < playerSequence.length; i += 1) {
+      if (playSequence[i] !== gameSequence[i]) {
+        return false;
       }
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
-      opacity += opacity * 0.1;
-  }, 10);
+    }
+  }
+  
+  return true;
+}
+
+function animate(element) {
+  var animateIntervalId;
+  var opacity = 0.1;  // initial opacity
+  console.log(typeof element, "<-typeof element", element, "<- element")
+  element.style.display = 'block'; // <- PROBLEM
+  
+  function fadeAnimation() {
+    if (opacity >= 1){
+        clearInterval(animateIntervalId);
+    }
+    
+    element.style.opacity = opacity;
+    element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
+    opacity += .5 * opacity;
+  }
+  
+  var animateIntervalId = setInterval(fadeAnimation, 10);
 }
