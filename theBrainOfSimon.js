efdedbe1db82729189030e7af8ec707ddd265309
Ian <ian (at) ianrobinson.net>
2018-05-05 12:12:39 +0200

    Move intervalId to global variable so the reset button can cancel the animateSequence interval sequence

--- theBrainOfSimon.js
+++ theBrainOfSimon.js
@@ -3,8 +3,9 @@ var redButton, blueButton, greenButton, yellowButton;
 var gameSequence = [];
 var playerSequence = [];
 var currentLevel = 0;
-var listenToPlayer = false;
+var listenToPlayer = true;
 var strictMode = false;
+var intervalId;
 
 window.onload = init;
 
@@ -34,13 +35,33 @@ function startGame() {
   levelSpace.innerHTML = "0";
   addToGameSequence();
   animateSequence(gameSequence);
-  gameLoop();
 }
 
+// Play the gameSequence
+function animateSequence(seq) {
+  var runCount = 0;
+  playerSequence = [];
+
+  function playSequence() {
+    // Clear interval after gameSequence plays to end
+    if (runCount >= seq.length - 1) {
+      clearInterval(intervalId);
+    }
+
+    // Animate the sequential buttons
+    animate(seq[runCount]);
+    runCount += 1;
+  }
+
+  intervalId = setInterval(playSequence, 1000);
+}
+
+// When player resets game
 function resetGame() {
   gameSequence = [];
   playerSequence = [];
-  currentLevel = 0;
+  levelSpace.innerHTML = "Press Start/Stop to Begin";
+  clearInterval(intervalId);
 }
 
 // Create a function which can add new random elements to an array.
@@ -50,21 +71,17 @@ function addToGameSequence() {
   gameSequence.push(newColor);
 }
 
-function gameLoop() {
-  // I'm not sure what this will do...
-}
-
 // What happens when a button is clicked
 function pressColorButton(color) {
-  if (listenToPlayer) {
+  console.log(listenToPlayer, "<-listenToPlayer");
+  if (listenToPlayer === true) {
     animate(color);
     playerSequence.push(color);
+    checkSequence();
   }
-
-  checkSequence();
 }
 
-// Functionality for the fade effect on colorButtons
+// How the animation works
 function animate(element) {
   var animateIntervalId;
 
@@ -82,32 +99,6 @@ function animate(element) {
   }
 
   animateIntervalId = setInterval(fadeAnimation, 10);
-}
-
-// Play the gameSequence
-function animateSequence(seq) {
-  var intervalId;
-  var runCount = 0;
-  
-  // Disable user input
-  listenToPlayer = false;
-  playerSequence = [];
-
-  function playSequence() {
-    // Clear interval after gameSequence plays to end
-    if (runCount >= seq.length - 1) {
-      clearInterval(intervalId);
-    }
-
-    // Animate the sequential buttons
-    animate(seq[runCount]);
-    runCount += 1;
-  }
-
-  intervalId = setInterval(playSequence, 1000);
-
-  // Enable user input after animate sequence played
-  listenToPlayer = true;
 }
 
 // This will test playerSequence against gameSequence
@@ -129,6 +120,7 @@ function checkSequence() {
   }
 }
 
+// Checks if playersSequence matches gameSequence
 function sequencesMatch() {
   for (var i = 0; i < playerSequence.length; i += 1) {
     if (playerSequence[i] !== gameSequence[i]) {
@@ -138,6 +130,7 @@ function sequencesMatch() {
   return true;
 }
 
+// The resulting code for when a player provides correct sequence
 function incrementSuccess() {
   playerSequence = [];
   addToGameSequence();
