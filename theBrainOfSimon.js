5bb9818dfcc25521797a42b2b2d6e0d612874058
Ian <ian (at) ianrobinson.net>
2018-05-05 11:14:40 +0200

    Clean up using JSLint

--- theBrainOfSimon.js
+++ theBrainOfSimon.js
@@ -10,25 +10,28 @@ window.onload = init;
 
 function init() {
   // Assign variables to buttons
-  redButton = document.getElementById('r');
-  blueButton = document.getElementById('b');
-  greenButton = document.getElementById('g');
-  yellowButton = document.getElementById('y');
-  
+  redButton = document.getElementById("r");
+  blueButton = document.getElementById("b");
+  greenButton = document.getElementById("g");
+  yellowButton = document.getElementById("y");
+
   // Completed Level Counter
-  levelSpace = document.getElementById('completedLevels');
-  
+  levelSpace = document.getElementById("completedLevels");
+
   // Assign variables to start and stop buttons
-  var startStopButton = document.getElementById('startStopButton');
-  var resetButton = document.getElementById('resetButton');
-  
+  var startStopButton = document.getElementById("startStopButton");
+  var resetButton = document.getElementById("resetButton");
+
   // This will be the operations board of the game
   startStopButton.onclick = startGame;
   resetButton.onclick = resetGame;
+
+  levelSpace.innerHTML = "Press Start/Stop to Begin";
 }
 
 // Manage what happens when user click Start/Stop
-function startGame() {  
+function startGame() {
+  levelSpace.innerHTML = "0";
   addToGameSequence();
   animateSequence(gameSequence);
   gameLoop();
@@ -48,7 +51,7 @@ function addToGameSequence() {
 }
 
 function gameLoop() {
-  
+  // I'm not sure what this will do...
 }
 
 // What happens when a button is clicked
@@ -57,28 +60,28 @@ function pressColorButton(color) {
     animate(color);
     playerSequence.push(color);
   }
-  
+
   checkSequence();
 }
 
 // Functionality for the fade effect on colorButtons
 function animate(element) {
   var animateIntervalId;
-  
+
   var opacity = 0.1;  // initial opacity
-  element.style.display = 'block';
-  
+  element.style.display = "block";
+
   function fadeAnimation() {
     if (opacity >= 1){
         clearInterval(animateIntervalId);
     }
-    
+
     element.style.opacity = opacity;
-    element.style.filter = 'alpha(opacity=' + opacity * 100 + ")";
-    opacity += .5 * opacity;
+    element.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    opacity += 0.5 * opacity;
   }
-  
-  var animateIntervalId = setInterval(fadeAnimation, 10);
+
+  animateIntervalId = setInterval(fadeAnimation, 10);
 }
 
 // Play the gameSequence
@@ -90,39 +93,54 @@ function animateSequence(seq) {
   listenToPlayer = false;
   playerSequence = [];
 
-  function playSequence() {    
+  function playSequence() {
     // Clear interval after gameSequence plays to end
     if (runCount >= seq.length - 1) {
       clearInterval(intervalId);
     }
-    
+
     // Animate the sequential buttons
     animate(seq[runCount]);
     runCount += 1;
   }
 
-  var intervalId = setInterval(playSequence, 1000);
-  
+  intervalId = setInterval(playSequence, 1000);
+
   // Enable user input after animate sequence played
   listenToPlayer = true;
 }
 
 // This will test playerSequence against gameSequence
 function checkSequence() {
+  console.log(gameSequence, "<-gameSequence");
+  console.log(playerSequence, "<-playerSequence");
+
   if (playerSequence.length === gameSequence.length) {
-    
-    // Check to see if playerSequence has errors compared to gameSequence 
+    // Check to see if playerSequence has errors compared to gameSequence
     for (var i = 0; i < playerSequence.length; i += 1) {
-      if (playerSequence[i] !== gameSequence[i]) {
-        console.log('Player Loses function');
-        return false;
+      if (!sequencesMatch()) {
+        console.log("Player Loses function");
+        levelSpace.innerHTML = "Game Over, You Achieved Level" + currentLevel + " | Congratulations";
+        resetGame();
       } else { // If they match, add color to gameSequence and animateSequence
-        playerSequence = [];
-        addToGameSequence();
-        currentLevel += 1;
-        levelSpace.innerHTML = currentLevel;
-        animateSequence(gameSequence);
+        incrementSuccess();
       }
     }
+}
+
+function sequencesMatch() {
+  for (var i = 0; i < playerSequence.length; i += 1) {
+    if (playerSequence[i] !== gameSequence[i]) {
+      return false;
+    }
   }
+  return true;
+}
+
+function incrementSuccess() {
+  playerSequence = [];
+  addToGameSequence();
+  currentLevel += 1;
+  levelSpace.innerHTML = currentLevel;
+  animateSequence(gameSequence);
 }
